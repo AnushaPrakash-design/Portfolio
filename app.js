793a39f9970cdbe0866ffb147664518eba1ff36d
Anusha <anusha.prapad@gmail.com>
2022-02-07 21:10:37 +0530

    feat: Landing phone done

--- app.js
+++ app.js
@@ -6,57 +6,25 @@
 // });
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var lastScrollTop;
-navbar = document.getElementById("navbar");
+let lastScrollTop;
+const navbar = document.getElementById("navbar");
+
+const navHeight = navbar.getBoundingClientRect().height;
+console.log(navHeight);
+
 window.addEventListener("scroll", function () {
 	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	//This line will get the location on scroll
 
 	if (scrollTop > lastScrollTop) {
 		//if it will be greater than the previous
-		navbar.style.top = "-100px";
+		navbar.style.top = "-" + navHeight + "px";
 		//set the value to the negetive of height of navbar
 	} else {
 		navbar.style.top = "0";
 	}
 	lastScrollTop = scrollTop; //New Position Stored
 });
-
-// window.onscroll = function () {
-// 	scrollFunction();
-// };
-
-// function scrollFunction() {
-// 	if (
-// 		document.body.scrollTop > 80 ||
-// 		document.documentElement.scrollTop > 80
-// 	) {
-// 		document.getElementById("navbar").style.padding = "4vh 8vw 2.4vh  ";
-// 	} else {
-// 		document.getElementById("navbar").style.padding = "4.92vh 8vw 2.4vh";
-// 	}
-// var mq = window.matchMedia("(min-width: 768px)");
-// if (mq.matches) {
-// 	document.getElementById("navbar").style.padding = "5vh 5.5vw 2.5vh  ";
-// } else {
-// 	document.getElementById("navbar").style.padding = "5vh 5.5vw 2.5vh";
-// }
-
-// document.documentElement?.clientHeight || window.innerHeight
-
-// const vH = document.documentElement?.clientHeight || window.innerHeight;
-// const landingSection = document.querySelector(".landing");
-// console.log(vH);
-// // landingSection.style.height = vH * 0.5;
-// landingSection.css
-
-// window.addEventListener("resize", function (event) {
-// 	// landingSection.style("height", vH + px);
-// 	console.log(vH);
-// 	console.log(landingSection);
-
-// });
-
 // landing page- 100vh
 
 const landSection = document.querySelector(".landing");
