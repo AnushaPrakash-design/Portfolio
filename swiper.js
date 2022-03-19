3a474a08193ba59a3a7b523ed27b9601c1463576
Anusha <anusha.prapad@gmail.com>
2022-03-19 18:18:06 +0530

    chore: Setup swiper

--- swiper.js
+++ swiper.js
@@ -1,6 +1,9 @@
 var swiper = new Swiper(".swiper_container", {
 	spaceBetween: 30,
 	effect: "fade",
+	loop: true,
+	speed: 400,
+	spaceBetween: 100,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
