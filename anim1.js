0a311e847e0217f03b5cfdefe47095e530ac7857
Anusha <anusha.prapad@gmail.com>
2022-04-11 11:31:11 +0530

    chore: Contact email and landing animation

--- anim1.js
+++ anim1.js
@@ -1,22 +1,34 @@
 const landingAnimation = gsap.timeline();
 
 landingAnimation
-	.from(".single_words", {
-		duration: 0.5,
+	.from(".landing_image", {
+		duration: 0.8,
+		scale: 1.05,
 		opacity: 0,
-		x: -30,
-		stagger: 0.6,
-		ease: "power2.easeInOut",
 	})
-	.from(
-		".main_heading",
-		{
-			duration: 1,
-			opacity: 0,
-			y: -50,
-			ease: "power2.easeInOut",
+	.from(".single_words h2", {
+		duration: 2,
+		opacity: 0,
+		// x: -30,
+		stagger: 0.2,
+		ease: "power2.easeOut",
+	});
 
-			// stagger: 0.6,
-		},
-		"-=2.2"
-	);
+const tl2 = gsap.timeline();
+
+tl2.from(".line1", {
+	duration: 0.8,
+	opacity: 0,
+	y: -20,
+	ease: "power1.easeOut",
+	delay: 0.3,
+}).from(
+	".line2",
+	{
+		duration: 0.8,
+		opacity: 0,
+		y: -20,
+		ease: "power1.easeOut",
+	},
+	"-=0.5"
+);
