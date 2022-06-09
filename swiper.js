df1e8264baf4b324cb015ae6dda8e453a2ed0c8b
Anusha <anusha.prapad@gmail.com>
2022-06-10 03:23:16 +0530

    images to torri added

--- swiper.js
+++ swiper.js
@@ -7,42 +7,42 @@ const closeBtn = document.querySelector(".swiper-close");
 // console.log("workBody", workBody);
 
 const imageClick = () => {
-	workBody.classList.toggle("no-scroll");
-	swiperWrap.classList.toggle("active");
-	console.log("hello");
+  workBody.classList.toggle("no-scroll");
+  swiperWrap.classList.toggle("active");
+  console.log("hello");
 };
 
 imagesArr.forEach((el, index) => {
-	el.addEventListener("click", imageClick);
+  el.addEventListener("click", imageClick);
 });
 
 var swiper = new Swiper(".swiper_container", {
-	spaceBetween: 30,
-	effect: "fade",
-	loop: true,
-	speed: 400,
-	grabCursor: true,
-	spaceBetween: 100,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  speed: 400,
+  grabCursor: true,
+  spaceBetween: 100,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
 
 // const imageClick
 // swiper.activeIndex();
 
 const closeClick = () => {
-	workBody.classList.remove("no-scroll");
-	swiperWrap.classList.remove("active");
+  workBody.classList.remove("no-scroll");
+  swiperWrap.classList.remove("active");
 };
 closeBtn.addEventListener("click", closeClick);
 
