26e53a7cb0fd5198750d1ed3b8bd192130025a00
Anusha <anusha.prapad@gmail.com>
2022-09-23 22:15:05 +0530

    feat: Tped.js assded to landing page

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
-	loader.style.display = "none";
-	// setTimeout(function () {
-	// 	preloader(secs + 1);
-	// }, 3000);
-	window.onload = function () {
-		setInterval(function () {
-			alert("Hello");
-		}, 10000);
-	};
+  loader.style.display = "none";
+  // setTimeout(function () {
+  // 	preloader(secs + 1);
+  // }, 3000);
+  window.onload = function () {
+    setInterval(function () {
+      alert("Hello");
+    }, 10000);
+  };
 });
 
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
@@ -21,17 +21,17 @@ const navHeight = navbar.getBoundingClientRect().height;
 console.log(navHeight);
 
 window.addEventListener("scroll", function () {
-	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-	//This line will get the location on scroll
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  //This line will get the location on scroll
 
-	if (scrollTop > lastScrollTop) {
-		//if it will be greater than the previous
-		navbar.style.top = "-" + navHeight + "px";
-		//set the value to the negetive of height of navbar
-	} else {
-		navbar.style.top = "0";
-	}
-	lastScrollTop = scrollTop; //New Position Stored
+  if (scrollTop > lastScrollTop) {
+    //if it will be greater than the previous
+    navbar.style.top = "-" + navHeight + "px";
+    //set the value to the negetive of height of navbar
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop; //New Position Stored
 });
 
 // hamburger menu
@@ -41,18 +41,18 @@ const hamburger = document.querySelector(".hamburger_open");
 const navMenu = document.querySelector(".hamburger_container");
 
 const handleHamClick = () => {
-	bodyDom.classList.toggle("no-scroll");
-	hamburger.classList.toggle("active");
-	navMenu.classList.toggle("active");
+  bodyDom.classList.toggle("no-scroll");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 };
 hamburger.addEventListener("click", handleHamClick);
 
 document.querySelectorAll(".nav_links").forEach((n) =>
-	n.addEventListener("click", () => {
-		bodyDom.classList.remove("no-scroll");
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-	})
+  n.addEventListener("click", () => {
+    bodyDom.classList.remove("no-scroll");
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
 );
 
 // landing page- 100vh
@@ -60,11 +60,11 @@ document.querySelectorAll(".nav_links").forEach((n) =>
 const landSection = document.querySelector(".landing");
 
 const measureHeight = () => {
-	return document.documentElement?.clientHeight || window.innerHeight;
+  return document.documentElement?.clientHeight || window.innerHeight;
 };
 
 const setHeight = (val) => {
-	landSection.style.height = val + "px";
+  landSection.style.height = val + "px";
 };
 
 console.log(measureHeight());
@@ -75,7 +75,16 @@ setHeight(vH);
 
 // On every resize
 window.addEventListener("resize", () => {
-	vH = measureHeight();
-	setHeight(vH);
-	// console.log(measureHeight());
+  vH = measureHeight();
+  setHeight(vH);
+  // console.log(measureHeight());
+});
+
+const typed = new Typed(".typed", {
+  strings: ["Design", "Code", "Life"],
+  typeSpeed: 100,
+  backspeed: 120,
+  loop: true,
+  smartBackspace: true,
+  fadeOut: true,
 });
