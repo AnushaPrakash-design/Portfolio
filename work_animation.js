72fa94e568c4f87068f4dd17d402cc8c85fe7a12
Anusha <anusha.prapad@gmail.com>
2022-09-30 23:44:49 +0530

    feat: Animation added to work page

---
+++ work_animation.js
@@ -0,0 +1,44 @@
+gsap.registerPlugin(ScrollTrigger);
+const landingAnimation = gsap.timeline();
+
+// Landing section Animation
+
+landingAnimation
+  .from(".landing_image", {
+    duration: 0.8,
+    scale: 1.05,
+    opacity: 0,
+  })
+  .from(".single_words h2", {
+    duration: 1,
+    opacity: 0,
+    // x: -30,
+    stagger: 0.2,
+    ease: "power2.easeOut",
+  })
+  .from(".content_wrapper", {
+    duration: 0.05,
+    scale: 1.05,
+    opacity: 0,
+  });
+
+const tl2 = gsap.timeline();
+
+tl2
+  .from(".line1", {
+    duration: 0.8,
+    opacity: 0,
+    y: -20,
+    ease: "power1.easeOut",
+    delay: 0.3,
+  })
+  .from(
+    ".line2",
+    {
+      duration: 0.8,
+      opacity: 0,
+      y: -20,
+      ease: "power1.easeOut",
+    },
+    "-=0.5"
+  );
