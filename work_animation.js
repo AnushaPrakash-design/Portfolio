d5ba27e2b457589ec6662afa3b4bae0701205f0a
Anusha <anusha.prapad@gmail.com>
2022-10-02 23:54:00 +0530

    feat: work info animation added

--- work_animation.js
+++ work_animation.js
@@ -1,21 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
-const landingAnimation = gsap.timeline();
+const WorkAnimation = gsap.timeline();
 
 // Work Landing section Animation
 
-landingAnimation
-  .from(".landing_image", {
-    duration: 0.8,
-    scale: 1.05,
-    opacity: 0,
-  })
-  .from(".single_words h2", {
-    duration: 1,
-    opacity: 0,
-    // x: -30,
-    stagger: 0.2,
-    ease: "power2.easeOut",
-  });
+WorkAnimation.from(".landing_image", {
+  duration: 0.8,
+  scale: 1.05,
+  opacity: 0,
+}).from(".single_words h2", {
+  duration: 1,
+  opacity: 0,
+  // x: -30,
+  stagger: 0.2,
+  ease: "power2.easeOut",
+});
 
 const tl2 = gsap.timeline();
 
@@ -45,8 +43,33 @@ tl3.from(".down_arrow", {
   opacity: 0,
   y: -10,
   repeat: 1000,
-  yolo: true,
   stagger: 0.2,
-  // ease: "power2.easeOut",
-  ease: "elastic.out(1, 0.3)",
+  ease: "back.out(1.7)",
 });
+
+// Project detail animation
+
+const tl4 = gsap.timeline({
+  defaults: {
+    duration: 0.8,
+    y: -20,
+    stagger: 0.2,
+    opacity: 0,
+    ease: "power2.easeOut",
+  },
+
+  scrollTrigger: {
+    trigger: ".project_details",
+    // markers: true,
+  },
+});
+
+tl4
+  .from(".about_food", {})
+
+  .from(".year", {}, "-=0.2")
+  .from(".tech", {}, "-=0.2")
+  .from(".livesite", {}, "-=0.2")
+  .from(".code", {}, "-=0.2");
+
+// project images animation
